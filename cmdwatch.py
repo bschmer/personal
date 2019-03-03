eeac504d2ca47970e26be60a3c0d3b2d875bcef6
Bryan Schmersal <bryan.schmersal@bossanova.com>
2019-03-03 10:57:21 -0500

    Fix up some of the pylint issues....watch() still needs some TLC

--- cmdwatch.py
+++ cmdwatch.py
@@ -17,8 +17,8 @@ TODO:
 '''
 
 import os
-import sys
 import os.path
+import sys
 import subprocess
 import time
 import itertools
@@ -47,14 +47,23 @@ def _output(msg, delta=None, ctime=None, term='\n'):
     sys.stdout.flush()
 
 
-class spin(object):
-    def __init__(self, spinchars='|/-\\', *args, **kwds):
+class IdleSpin(object):
+    '''
+    Simple class to show that things are happening, i.e. we're not stuck
+    '''
+    def __init__(self, spinchars='|/-\\', mincycle=.1):
+        '''
+        Initialize the spinner
+        '''
         self._chars = itertools.cycle(spinchars)
         self._lastspin = 0
         self._lastout = time.time()
-        self._mincycle = .1
+        self._mincycle = mincycle
 
     def spin(self):
+        '''
+        Display the indicator
+        '''
         ctime = time.time()
         if self._lastspin + self._mincycle > ctime:
             return
@@ -62,29 +71,46 @@ class spin(object):
         self._lastspin = ctime
 
     def output(self, msg, *args, **kwds):
+        '''
+        Not really sure how this is different than spin() aside from the rate limiter.
+        '''
         ctime = time.time()
-        _output(msg, ctime = ctime, delta = ctime - self._lastout, *args, **kwds)
+        _output(msg, ctime=ctime, delta=ctime - self._lastout, *args, **kwds)
         self._lastout = ctime
 
 
-def showdiff(a, b):
-    if not a:
-        return b
-    if not b:
-        return '%s>' % b
-    if a == b:
-        return a
-    return '%s->%s' % (a,b)
+def showdiff(item0, item1):
+    '''
+    Show when there's a difference between two values
+    '''
+    if not item0:
+        return item1
+    if not item1:
+        return '%s>' % item1
+    if item0 == item1:
+        return item0
+    return '%s->%s' % (item0, item1)
 
 class Key(object):
-    def __init__(self, key, *args, **kwds):
+    '''
+    Handle substitutions
+    '''
+    def __init__(self, key):
         self._key = string.Template(key)
 
     def handle(self, *args, **kwds):
-        d = dict([('p%s' % x[0], x[1]) for x in  enumerate(args)])
-        return self._key.substitute(d)
+        '''
+        Do the substitutions.
+        '''
+        data = dict()
+        data.update(kwds)
+        data.update(dict([('p%s' % x[0], x[1]) for x in  enumerate(args)]))
+        return self._key.substitute(data)
 
-class genout(object):
+class GenOut(object):
+    '''
+    Generate output
+    '''
     def __init__(self, cmd, sleeptime=1):
         self._cmd = cmd
         self._sleeptime = sleeptime
@@ -94,7 +120,7 @@ class genout(object):
         ctime = ltime - self._sleeptime
         timehist = []
         while True:
-            if len(timehist) > 0:
+            if timehist:
                 timehist.append(self._sleeptime - (ltime - ctime))
             else:
                 timehist.append(0)
@@ -105,27 +131,32 @@ class genout(object):
             deadline = ctime + self._sleeptime + avg
             proc = subprocess.Popen(self._cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-            for l in iter(proc.stdout.readline, b''):
-                yield l
+            for outputline in iter(proc.stdout.readline, b''):
+                yield outputline
             time.sleep(deadline - time.time())
             ltime = time.time()
 
     def sleeptime(self, sleeptime):
+        '''
+        Set the sleep time.
+        '''
         self._sleeptime = sleeptime
 
-def watch(cmd, key, comments=False, resolvepid=None, tstamp=None, maxsplit=10000, pidreplace=None, sleeptime=1, iterations=0):
-
-    spinner = spin()
+def watch(cmd, key, comments=False, resolvepid=None, tstamp=None,
+          maxsplit=10000, pidreplace=None, sleeptime=1, iterations=0):
+    '''
+    Watch the output from a command.
+    '''
+    spinner = IdleSpin()
     timestamp = ''
     running = []
     history = {}
     cmds = {}
     keyhandler = Key(key)
 
-    outgen = genout(cmd, sleeptime=sleeptime)
-    for index, l in enumerate(outgen):
-        r = select.select([sys.stdin], [], [], .01)[0]
-        if r:
+    outgen = GenOut(cmd, sleeptime=sleeptime)
+    for index, outputline in enumerate(outgen):
+        if select.select([sys.stdin], [], [], .01)[0]:
             cmd = sys.stdin.readline().strip().lower()
             if cmd.startswith('q'):
                 # Exit app
@@ -134,60 +165,61 @@ def watch(cmd, key, comments=False, resolvepid=None, tstamp=None, maxsplit=10000
                 parts = cmd.split()
                 try:
                     outgen.sleeptime(int(parts[1]))
-                except Exception, e:
-                    print str(e)
+                except ValueError:
+                    print 'Invalid value: %s' % parts[1]
             elif cmd.startswith('i'):
                 parts = cmd.split()
                 try:
                     iterations = int(parts[1])
-                except Exception, e:
-                    print str(e)
+                except ValueError:
+                    print 'Invalid value: %s' % parts[1]
             else:
                 print 'Unknown command: %s' % cmd
         if iterations and index > iterations:
             break
         spinner.spin()
-        if l.startswith('#') and not comments:
+        if outputline.startswith('#') and not comments:
             continue
-        ld = l.strip().split(None, maxsplit)
-        curkey = keyhandler.handle(*ld)
+        linetokens = outputline.strip().split(None, maxsplit)
+        curkey = keyhandler.handle(*linetokens)
 
-        if resolvepid and len(ld) > resolvepid:
-            pid = ld[resolvepid]
+        if resolvepid and len(linetokens) > resolvepid:
+            pid = linetokens[resolvepid]
             if os.path.exists('/proc/%s' % pid):
                 if pid not in cmds:
                     try:
                         fullcmd = open('/proc/%s/cmdline' % pid).read().replace('\0', ' ')
-                        #spinner.output(fullcmd)
-                    except Exception, e:
-                        print e
+                    except IOError:
                         fullcmd = cmd
                     cmds[pid] = fullcmd
                 cmd = cmds[pid]
                 loc = resolvepid
-                if pidreplace and len(ld) > pidreplace:
+                if pidreplace and len(linetokens) > pidreplace:
                     loc = pidreplace
-                ld[loc] = cmd
+                linetokens[loc] = cmd
 
-        if tstamp is not None and ld[tstamp] != timestamp:
-            timestamp = ld[tstamp]
+        if tstamp is not None and linetokens[tstamp] != timestamp:
+            timestamp = linetokens[tstamp]
             # Clean up any stragglers
-            for k in history.keys():
+            for k in history:
                 if k not in running:
                     history.pop(k)
 
-        if tstamp is not None and len(ld) > tstamp:
-            ld.pop(tstamp)
+        if tstamp is not None and len(linetokens) > tstamp:
+            linetokens.pop(tstamp)
         if not curkey in running:
             running.append(curkey)
         if curkey not in history:
-            history[curkey] = [''] * 8  # The number doesn't really matter....could be len(ld)
-        if ld != history[curkey]:
-            spinner.output(' '.join([showdiff(*x) for x in zip(history[curkey], ld)]))
-            history[curkey] = ld
+            history[curkey] = [''] * 8  # The number doesn't really matter....could be len(linetokens)
+        if linetokens != history[curkey]:
+            spinner.output(' '.join([showdiff(*x) for x in zip(history[curkey], linetokens)]))
+            history[curkey] = linetokens
 
 
-if __name__ == '__main__':
+def main():
+    '''
+    Main program....it's here so that the silly lint rules are masked
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', action='store', dest='key', default='$p0')
     parser.add_argument('-p', action='store', dest='pid', default=None)
@@ -199,12 +231,14 @@ if __name__ == '__main__':
     parser.add_argument('rest', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
-    cmd = ' '.join(args.rest)
     if args.pid:
         args.pid = int(args.pid)
     if args.pidreplace:
         args.pidreplace = int(args.pidreplace)
     if args.tstamp:
         args.tstamp = int(args.tstamp)
-    watch(args.rest, args.key, comments=args.comments, resolvepid=args.pid, tstamp=args.tstamp, pidreplace=args.pidreplace, sleeptime=args.interval, iterations=args.iterations)
+    watch(args.rest, args.key, comments=args.comments, resolvepid=args.pid, tstamp=args.tstamp,
+          pidreplace=args.pidreplace, sleeptime=args.interval, iterations=args.iterations)
 
+if __name__ == '__main__':
+    main()
